0e0fc38129bf91e5968a432e797e77a3c411babb
Asad Khan <asad@macworldtechnology.com>
2023-08-30 01:43:03 -0700

    change

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-require('dotenv').config()
-require('./config/db');
 const express = require('express');
-const cors = require('cors');
-const users = require('./routes/users');
-const trade = require('./routes/trade')
-const morgan = require('morgan')
+
 const app = express();
-const yaml = require('yamljs')
-const swaggerJsdoc = yaml.load('./config/swagger.yaml')
-const swaggerUI = require('swagger-ui-express')
-const {errorHandler} = require('./handler/errorHandler')
 
+app.get('/', (req, res) => res.send('Home Page Route'));
 
-// app.use('/api/docs',swaggerUI.serve,swaggerUI.setup(swaggerJsdoc))
-// app.set('view engine','ejs')
-// app.use(morgan('dev'))
-app.use(cors({ origin: true, credentials: true }));
-app.use(express.json());
+app.get('/about', (req, res) => res.send('About Page Route'));
 
-app.get('/', (req, res) => {
-  res.send('Hello ibbi');
-});
+app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
-app.use('/users', users);
-app.use('/trade', trade);
+app.get('/contact', (req, res) => res.send('Contact Page Route'));
 
-app.use(errorHandler)
-const port = process.env.PORT || 3005;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
